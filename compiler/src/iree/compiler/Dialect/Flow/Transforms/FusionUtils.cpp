1213882aa4227186bdc1cc3928a4f95879d68060
Ian Wood <ianwood2024@u.northwestern.edu>
2024-06-25 00:07:35 +0000

    save
    Signed-off-by: Ian Wood <ianwood2024@u.northwestern.edu>

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/FusionUtils.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/FusionUtils.cpp
@@ -9,7 +9,9 @@
 
 #include "compiler/src/iree/compiler/Dialect/Flow/Transforms/FusionUtils.h"
 #include "compiler/src/iree/compiler/Dialect/Flow/Transforms/RegionOpUtils.h"
+#include "llvm/ADT/STLExtras.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
 
 namespace mlir::iree_compiler::IREE::Flow {
 
@@ -97,6 +99,129 @@ bool areFusableAsElementwiseOps(MLIRContext *context, OpOperand *fusedOperand,
 
   // All other cases dont fuse.
   return false;
+}
+
+LogicalResult ExpansionInfo::compute(
+    LinalgExt::LinalgFusionOpInterface fusibleOp, OpOperand *fusableOpOperand,
+    ArrayRef<AffineMap> reassociationMaps, ArrayRef<int64_t> expandedShape,
+    ArrayRef<int64_t> collapsedShape, PatternRewriter &rewriter) {
+  if (reassociationMaps.empty())
+    return failure();
+  AffineMap fusedIndexMap = fusibleOp.getMatchingIndexingMap(fusableOpOperand);
+
+  SmallVector<int64_t, 4> originalLoopRange = fusibleOp.getStaticLoopRanges();
+  originalLoopExtent.assign(originalLoopRange.begin(), originalLoopRange.end());
+
+  reassociation.clear();
+  expandedShapeMap.clear();
+  // Compute the number of dimension in the expanded op that correspond to each
+  // dimension of the original op.
+  SmallVector<unsigned> numExpandedDims(fusedIndexMap.getNumDims(), 1);
+  expandedShapeMap.resize(fusedIndexMap.getNumDims());
+  for (const auto &resultExpr : llvm::enumerate(fusedIndexMap.getResults())) {
+    unsigned pos = cast<AffineDimExpr>(resultExpr.value()).getPosition();
+    AffineMap foldedDims = reassociationMaps[resultExpr.index()];
+    numExpandedDims[pos] = foldedDims.getNumResults();
+    ArrayRef<int64_t> shape =
+        expandedShape.slice(foldedDims.getDimPosition(0), numExpandedDims[pos]);
+    expandedShapeMap[pos].assign(shape.begin(), shape.end());
+  }
+  // The remaining dimensions remain the same.
+  for (unsigned i : llvm::seq<unsigned>(0, fusedIndexMap.getNumDims()))
+    if (expandedShapeMap[i].empty())
+      expandedShapeMap[i] = {originalLoopExtent[i]};
+
+  // Compute reassociation map from the original op to the expanded op.
+  unsigned sum = 0;
+  reassociation.reserve(fusedIndexMap.getNumDims());
+  for (const auto &numFoldedDim : llvm::enumerate(numExpandedDims)) {
+    auto seq = llvm::seq<int64_t>(sum, sum + numFoldedDim.value());
+    reassociation.emplace_back(seq.begin(), seq.end());
+    sum += numFoldedDim.value();
+  }
+  expandedOpNumDims = sum;
+  return success();
+}
+
+unsigned ExpansionInfo::getOrigOpNumDims() const {
+  return reassociation.size();
+}
+unsigned ExpansionInfo::getExpandedOpNumDims() const {
+  return expandedOpNumDims;
+}
+ReassociationIndicesRef ExpansionInfo::getExpandedDims(unsigned i) const {
+  return reassociation[i];
+}
+ArrayRef<int64_t> ExpansionInfo::getExpandedShapeOfDim(unsigned i) const {
+  return expandedShapeMap[i];
+}
+ArrayRef<int64_t> ExpansionInfo::getOriginalShape() const {
+  return originalLoopExtent;
+}
+
+LogicalResult isOpExpandable(LinalgExt::LinalgFusionOpInterface fusibleOp,
+                             const ExpansionInfo &expansionInfo,
+                             PatternRewriter &rewriter) {
+  linalg::LinalgOp maybeLinalgOp =
+      dyn_cast<linalg::LinalgOp>(fusibleOp.getOperation());
+  if (maybeLinalgOp && !maybeLinalgOp.hasIndexSemantics())
+    return success();
+  for (unsigned i : llvm::seq<unsigned>(0, expansionInfo.getOrigOpNumDims())) {
+    ArrayRef<int64_t> expandedShape = expansionInfo.getExpandedShapeOfDim(i);
+    if (expandedShape.size() == 1)
+      continue;
+    for (int64_t shape : expandedShape.drop_front()) {
+      if (ShapedType::isDynamic(shape)) {
+        return rewriter.notifyMatchFailure(
+            fusibleOp, "cannot expand due to index semantics and dynamic dims");
+      }
+    }
+  }
+  return success();
+}
+
+AffineMap getIndexingMapInExpandedOp(OpBuilder &builder, AffineMap indexingMap,
+                                     const ExpansionInfo &expansionInfo) {
+  SmallVector<AffineExpr> newExprs;
+  for (AffineExpr expr : indexingMap.getResults()) {
+    unsigned pos = cast<AffineDimExpr>(expr).getPosition();
+    SmallVector<AffineExpr, 4> expandedExprs = llvm::to_vector<4>(
+        llvm::map_range(expansionInfo.getExpandedDims(pos), [&](int64_t v) {
+          return builder.getAffineDimExpr(static_cast<unsigned>(v));
+        }));
+    newExprs.append(expandedExprs.begin(), expandedExprs.end());
+  }
+  return AffineMap::get(expansionInfo.getExpandedOpNumDims(),
+                        indexingMap.getNumSymbols(), newExprs,
+                        builder.getContext());
+}
+
+RankedTensorType getExpandedType(RankedTensorType originalType,
+                                 AffineMap indexingMap,
+                                 const ExpansionInfo &expansionInfo) {
+  SmallVector<int64_t> expandedShape;
+  for (AffineExpr expr : indexingMap.getResults()) {
+    unsigned dim = cast<AffineDimExpr>(expr).getPosition();
+    auto dimExpansion = expansionInfo.getExpandedShapeOfDim(dim);
+    expandedShape.append(dimExpansion.begin(), dimExpansion.end());
+  }
+  return RankedTensorType::get(expandedShape, originalType.getElementType());
+}
+
+SmallVector<ReassociationIndices>
+getReassociationForExpansion(AffineMap indexingMap,
+                             const ExpansionInfo &expansionInfo) {
+  SmallVector<ReassociationIndices> reassociation;
+  unsigned numReshapeDims = 0;
+  for (AffineExpr expr : indexingMap.getResults()) {
+    unsigned dim = cast<AffineDimExpr>(expr).getPosition();
+    auto numExpandedDims = expansionInfo.getExpandedDims(dim).size();
+    SmallVector<int64_t, 2> indices = llvm::to_vector<2>(
+        llvm::seq<int64_t>(numReshapeDims, numReshapeDims + numExpandedDims));
+    reassociation.emplace_back(std::move(indices));
+    numReshapeDims += numExpandedDims;
+  }
+  return reassociation;
 }
 
 } // namespace mlir::iree_compiler::IREE::Flow
--- compiler/src/iree/compiler/Dialect/Flow/Transforms/FusionUtils.h
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/FusionUtils.h
@@ -10,7 +10,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/IR/Operation.h"
+#include "iree/compiler/Dialect/LinalgExt/IR/LinalgExtInterfaces.h"
+#include "mlir/Dialect/Utils/ReshapeOpsUtils.h"
+#include "mlir/IR/PatternMatch.h"
 
 namespace mlir::iree_compiler::IREE::Flow {
 
@@ -19,4 +21,70 @@ namespace mlir::iree_compiler::IREE::Flow {
 bool areFusableAsElementwiseOps(MLIRContext *context, OpOperand *operand,
                                 bool fuseMultiReduction);
 
+/// Information needed to expand a `LinalgExt::LinalgFusionOpInterface`
+/// operation and fold a reshape with it. This is a more generalized version of
+/// `ExpansionInfo` in Linalg/Transforms/ElementwiseOpFusion.cpp that operates
+/// on an interface
+class ExpansionInfo {
+public:
+  // Computes the mapping from original dimensions of the op to the dimensions
+  // of the expanded op given the `indexingMap` of the fused operand/result of
+  // op, the `reassocationMaps` of the reshape op and the shape of
+  // the expanded op.
+  LogicalResult compute(LinalgExt::LinalgFusionOpInterface fusibleOp,
+                        OpOperand *fusableOpOperand,
+                        ArrayRef<AffineMap> reassociationMaps,
+                        ArrayRef<int64_t> expandedShape,
+                        ArrayRef<int64_t> collapsedShape,
+                        PatternRewriter &rewriter);
+  unsigned getOrigOpNumDims() const;
+  unsigned getExpandedOpNumDims() const;
+  ReassociationIndicesRef getExpandedDims(unsigned i) const;
+  ArrayRef<int64_t> getExpandedShapeOfDim(unsigned i) const;
+  ArrayRef<int64_t> getOriginalShape() const;
+
+private:
+  /// Reassociation from the dimensions in the original operation to the
+  /// dimension of the expanded operation.
+  SmallVector<ReassociationIndices> reassociation;
+  /// Mapping from extent of loops in the original operation, to the extent of
+  /// loops in the expanded operation.
+  SmallVector<SmallVector<int64_t>> expandedShapeMap;
+  /// Extent of the loop in the original operation.
+  SmallVector<int64_t> originalLoopExtent;
+  unsigned expandedOpNumDims;
+};
+
+/// Expanding the body of an operation requires adaptations of the
+/// accessed loop indices. Specifically, access of indices in the original
+/// operation need to be replaced with linearizations of indices in the expanded
+/// op. That requires the shape of the expanded dimensions to be static (at
+/// least all but the most significant). For now check that these are all
+/// statically sized. Note that this could be extended to handle dynamic case,
+/// but the implementation below uses `affine.apply` which seems to have issues
+/// when the shapes are not static.
+LogicalResult isOpExpandable(LinalgExt::LinalgFusionOpInterface fusibleOp,
+                             const ExpansionInfo &expansionInfo,
+                             PatternRewriter &rewriter);
+
+/// Return the indexing map to use in the expanded op for a given the
+/// `indexingMap` of the original operation.
+AffineMap getIndexingMapInExpandedOp(OpBuilder &builder, AffineMap indexingMap,
+                                     const ExpansionInfo &expansionInfo);
+
+/// Return the type of the operand/result to use in the expanded op given
+/// the type in the original op.
+RankedTensorType getExpandedType(RankedTensorType originalType,
+                                 AffineMap indexingMap,
+                                 const ExpansionInfo &expansionInfo);
+
+/// Returns the reassociation maps to use in the `tensor.expand_shape`
+/// operation to convert the operands of the original operation to operands of
+/// the expanded operation. The same method is used to compute the
+/// `tensor.collapse_shape` used to collapse the result of the expanded
+/// op to get the value that can replace all uses of the results of the original
+/// op.
+SmallVector<ReassociationIndices>
+getReassociationForExpansion(AffineMap indexingMap,
+                             const ExpansionInfo &expansionInfo);
 } // namespace mlir::iree_compiler::IREE::Flow
