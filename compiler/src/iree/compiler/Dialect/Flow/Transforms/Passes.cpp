fa85e7330c494085824659140a32e2682131a421
MaheshRavishankar <mahesh.ravishankar@gmail.com>
2024-07-09 09:28:48 -0700

    [Flow] Generalize horizontal contraction fusion to cover more cases.
    Current implementation of horizontal fusion worked only for
    contraction (-> truncf)? cases. To generalize it to more cases the
    following changes were needed
    
    1) Instead of looking for a `linalg.generic` with a single
       `arith.truncf`, use `isBitTruncate` utility method.
    
    2) Use `Operation::Equivalence` to check that the contraction
       operations are "similar" and any subsequent truncation operations
       are "similar" too.
    
    3) Instead of trying to find an insertion point based on existing
       dominance relationship between operations,
       - Always insert the horizontally fused contraction before the first
         contraction
       - Always insert the horizontally fused truncation operation before
         the first truncation operation
    
    4) Instead of generating the fills/empty along with horizontally fused
       operations, use separate patterns to fold concats of fills and
       concats of emptys into fill and empty, respectively.
    
    Signed-off-by: MaheshRavishankar <mahesh.ravishankar@gmail.com>

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/Passes.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/Passes.cpp
@@ -191,7 +191,7 @@ void addDispatchRegionCreationPreprocessingPasses(OpPassManager &passManager) {
       //    producer-consumer fusion.
       .addPass(IREE::Flow::createSinkReshapesPass)
       .addPass(IREE::Flow::createCanonicalizerPass)
-    .addPass(mlir::createCSEPass);
+      .addPass(mlir::createCSEPass);
 
   if (clEnableFuseHorizontalContractions) {
     FunctionLikeNest(passManager)
